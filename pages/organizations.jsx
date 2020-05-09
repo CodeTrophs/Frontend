7ad79bbfedaf063d5b5627214927e085e15f5afe
alizahir23 <alizahiruddin@gmail.com>
2020-05-09 22:42:17 +0530

    header component added

---
+++ pages/organizations.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../src/components/Header';
+
+const organizations = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default organizations;
