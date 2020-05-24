d962c188f291d9ee996383aa1f67b714e8c99060
Vipul Sharma <vipulsnp@gmail.com>
2020-05-24 22:23:19 +0530

    Added Login/ Signup using firebase

--- src/components/Home/WelcomeComponent.jsx
+++ src/components/Home/WelcomeComponent.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
 
 import styles from '../../css/home.module.css';
+import firebase from '../../firebase'
 
 export default function WelcomeComponent() {
+
+
+  const handleGoogleSignIn = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    /*
+        Define Required  Scopes here
+    */
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      //  Access Token for using Google API's
+      const token = result.credential.accessToken;
+      //  Signed-in user info.
+      const { user } = result;
+      // eslint-disable-next-line no-console
+      console.log("Token : ", token);
+      // eslint-disable-next-line no-console
+      console.log("User : ", user);
+
+      /*
+          Redirect to the required page here 
+      */
+
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    });
+  }
+
+  const handleGithubSignIn = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GithubAuthProvider();
+
+
+    /*
+       Define Required  Scopes here
+   */
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      //  Access Token for using github api
+      const token = result.credential.accessToken;
+      //  Signed-in user info.
+      const { user } = result;
+      // eslint-disable-next-line no-console
+      console.log(user, token);
+
+      /*
+          Redirect to the required Page here 
+      */
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    });
+  }
+
   return (
     <div className={styles['welcome-container']}>
       <div className={styles['welcome-left']}>
@@ -17,7 +74,7 @@ export default function WelcomeComponent() {
         </p>
 
         <div className={styles['sign-in-buttons']}>
-          <button className={styles['github-button']} type="submit">
+          <button className={styles['github-button']} type="submit" onClick={handleGithubSignIn}>
             <img
               alt="Icon-awesome-github.png"
               src="/images/Iconawesome-github.png"
@@ -26,7 +83,7 @@ export default function WelcomeComponent() {
             <img alt="Right-Arrow.svg" src="/icons/arrow-right.png" />
           </button>
 
-          <button className={styles['google-button']} type="submit">
+          <button className={styles['google-button']} type="submit" onClick={handleGoogleSignIn}>
             <img alt="Icon-simple-google" src="/images/google.svg" />
             <p>Sign in with Google</p>
             <img alt="Right-Arrow.svg" src="/icons/arrow-right.png" />
