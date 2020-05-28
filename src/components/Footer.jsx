c0b26585445c1fefdf08141bbd6d4f7bef9f3b26
Vipul Sharma <vipulsnp@gmail.com>
2020-05-28 16:04:27 +0530

    Redesigned Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,42 +5,49 @@ import styles from '../css/footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles['left-col']}>
-        <h2>Contact Us</h2>
-        <div className={styles['left-badges']}>
-          <div>
-            <img src="/SVG/code-of-conduct.svg" alt="thumb" />
-            Code of Conduct
-          </div>
-          <div>
-            <img src="/SVG/forum.svg" alt="forum" />
-            Forum
+      <span className={styles['mobile-head']}>
+        <h1>Contact Us<hr />  </h1><img src="/logo/monocolored-logo.png" alt="pen" />
+      </span>
+      <div className={styles['upper-row']}>
+        <div className={styles['left-col']}>
+          <h1>Contact Us<hr />  </h1>
+          <div className={styles['left-badges']}>
+            <div>
+              <img src="/SVG/twitter.svg" alt="twitter" />
+              @OPENSRCDESIGN
+            </div>
+            <div>
+              <img src="/SVG/Github.svg" alt="github" />
+              Github
+            </div>
+            <div>
+              <img src="/SVG/open_collective.svg" alt="github" />
+              Open Collective
+            </div>
+            <div>
+                <img src="/SVG/forum.svg" alt="forum" />
+              Forum
+            </div>
+            <div>
+              <img src="/SVG/code-of-conduct.svg" alt="thumb" />
+              Code of Conduct
+            </div>
           </div>
         </div>
-        <div
-          style={{
-            display: 'flex',
-            color: 'white',
-            fontWeight: '200',
-            cursor: 'pointer'
-          }}>
+        <div className={styles['right-col']}>
+          <img src="/logo/monocolored-logo.png" alt="pen" />
+        </div>
+      </div>
+                    {/* Bottom Row */}
+      <div className={styles['bottom-row']}>
+        <div>
           <img src="/SVG/pencil.svg" alt="pencil" />
-          <p>Edit this page</p>
+          <p> Edit this page </p>
         </div>
-      </div>
-      <div className={styles['center-col']}>
-        <div className={styles.stripes} style={{ backgroundColor: '#00FFF2' }}>
-          <img src="/SVG/code.svg" alt="</>" />
+        <div>
+          <img src="/SVG/code.svg" alt="</>"/>
+          <p> View Source Code </p> 
         </div>
-        <div className={styles.stripes} style={{ backgroundColor: '#797979' }}>
-          <img src="/SVG/Github.svg" alt="github" />
-        </div>
-        <div className={styles.stripes} style={{ backgroundColor: '#5B92AA' }}>
-          <img src="/SVG/twitter.svg" alt="twitter" />
-        </div>
-      </div>
-      <div className={styles['right-col']}>
-        <img src="/logo/monocolored-logo.png" alt="pen" />
       </div>
     </footer>
   );
