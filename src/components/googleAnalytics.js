42927244a40def620287474ba10168da079a4da2
Irshad Ansari <irshadjsr21@gmail.com>
2020-07-19 13:16:16 +0530

    [Feat] Added google analytics

--- src/components/googleAnalytics.js
+++ src/components/googleAnalytics.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
@@ -8,3 +9,15 @@ export const logPageView = () => {
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 };
+=======
+import ReactGA from "react-ga"
+
+export const initGA = () => {
+  ReactGA.initialize("UA-173001988-1")
+}
+
+export const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
+>>>>>>> [Feat] Added google analytics
