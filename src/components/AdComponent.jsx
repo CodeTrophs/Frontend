ce194e1d42056c12b343c1a5f1b6aeeb01e4467e
Vipul Sharma <vipulsnp@gmail.com>
2020-08-05 18:10:23 +0530

    Changed  id for adsense

--- src/components/AdComponent.jsx
+++ src/components/AdComponent.jsx
@@ -5,8 +5,8 @@ export default function AdDisplay() {
 
   return (
     <AdSense.Google
-      client="ca-pub-5275574079376570"
-      slot="7806394673"
+      client="ca-pub-3450125087548369"
+      slot="4337088929"
       format="auto"
       responsive="true"
       layoutKey="-gw-1+2a-9x+5c"
