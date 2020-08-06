77e76f340cdc1adbc83f526694894b5b5cbba29c
Irshad Ansari <irshadjsr21@gmail.com>
2020-08-06 12:24:44 +0530

    Fixed conflict

--- src/components/googleAnalytics.js
+++ src/components/googleAnalytics.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
@@ -10,20 +8,3 @@ export const logPageView = () => {
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 };
-=======
-=======
->>>>>>> [Feat] Added google analytics
-import ReactGA from "react-ga"
-
-export const initGA = () => {
-  ReactGA.initialize("UA-173001988-1")
-}
-
-export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname })
-  ReactGA.pageview(window.location.pathname)
-}
-<<<<<<< HEAD
->>>>>>> [Feat] Added google analytics
-=======
->>>>>>> [Feat] Added google analytics
