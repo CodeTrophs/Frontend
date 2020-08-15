6425dc8943216974fd567797d7dd27f6a2baf25d
Vipul Sharma <vipulsnp@gmail.com>
2020-08-15 22:55:02 +0530

    Minor Changes UI

--- src/FormValidation.js
+++ src/FormValidation.js
@@ -41,6 +41,10 @@ export async function checkUserName(userName, uid) {
 
   const limitCheck = checkLengthLimit(userName.length, 50, 1);
   const userNamePattern = new RegExp(/^[a-z0-9_]{1,50}$/);
+  if (limitCheck !== null)
+  {
+    return limitCheck;
+    }
   if ((userName.match(userNamePattern) === null) && limitCheck === null) {                // Pattern validation to include only Alphabets , Numbers and underscore(_)
     return "Username can only contain alphabets, numbers and underscore(_).";
   }
