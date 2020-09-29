de82f375af8987fc8ac84dd07d8e632e0ab46b50
Vipul Sharma <vipulsnp@gmail.com>
2020-09-29 23:35:03 +0530

    Changed organisations page

--- pages/organizations.jsx
+++ pages/organizations.jsx
@@ -1,54 +1,14 @@
 import React from 'react';
 
-import AdDisplay from '../src/components/AdComponent';
+// import AdDisplay from '../src/components/AdComponent';
 import Header from '../src/components/Header';
 import LandingContainer from '../src/components/LandingContainer';
 import Organisation from '../src/components/Organisation';
 import SearchBar from '../src/components/SearchBar';
 import styles from '../src/scss/organisations.module.scss';
+import Organisations from '../src/services/organisations.json';
 
 const organizations = () => {
-  const Organisations = [
-    {
-      organisation: 'Adobe',
-      id: 1,
-      link: 'alizahir.github.io',
-      orgDescription:
-        'Description of organisation. Random texts coming here and there. Lets enter something to fill these spaces. This is just a sample of the text that  something to fill these spaces. This is just a sample of the text that  something to fill these spaces. This is just a sample of the text that  something to fill these spaces. This is just a sample of the text that  something to fill these spaces. This is just a sample of the text that is to be entered into the boxes.',
-      langTags: ['React', 'Javascript'],
-      topicTags: ['Topic 1', 'Topic 2'],
-      github: 'http://www.github.com',
-      slack: 'https://www.slack.com',
-      bgurl: '/SVG/org-bg1.png'
-    },
-    {
-      organisation: 'Facebook',
-      id: 2,
-      link: 'nandana.github.io',
-
-      orgDescription:
-        'Description of organisation. Random texts coming here and there. Lets enter something to fill these spaces. This is just a sample of the text that is to be entered into the boxes.',
-      langTags: ['React', 'Javascript'],
-      topicTags: ['Topic 1', 'Topic 2'],
-      github: 'http://www.github.com',
-      slack: 'https://www.slack.com',
-      bgurl: '/SVG/org-bg1.png'
-    },
-    {
-      organisation: 'Mozilla',
-      id: 3,
-      link: 'arya.github.io',
-
-      orgDescription:
-        'Description of organisation. Random texts coming here and there. Lets enter something to fill these spaces. This is just a sample of the text that is to be entered into the boxes.',
-      langTags: ['React', 'Javascript'],
-      topicTags: ['Topic 1', 'Topic 2'],
-      github: 'http://www.github.com',
-      slack: 'https://www.slack.com',
-      bgurl: '/SVG/org-bg1.png'
-    }
-  ];
-
   return (
     <div>
       <Header />
@@ -59,14 +19,16 @@ const organizations = () => {
         line3="Search below to narrow down your results."
         imgsrc="/SVG/organisation-imgsrc.png"
       />
-      <AdDisplay />
+      {/* <AdDisplay /> */}
       <SearchBar page="organizations" />
       <div className={styles['organisations-grid']}>
-        {Organisations.map((organisation) => (
+        {Organisations.data.map((organisation) => (
           <Organisation
             className={styles.card}
-            key={organisation.id}
+            key={organisation.name + organisation.topics}
             org={organisation}
+            tech={organisation.technologies.split('|')}
+            topics={organisation.topics.split('|')}
           />
         ))}
       </div>
--- src/components/Organisation.jsx
+++ src/components/Organisation.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 
+import styles from '../scss/organisations.module.scss';
 import Modal from './Modal';
 
-const organisation = ({ org }) => {
+const organisation = ({ org, tech, topics }) => {
   const [ModalShow, setModalShow] = useState(false);
 
   const showModal = () => {
@@ -14,122 +15,66 @@ const organisation = ({ org }) => {
     }
   };
   return (
-    <div
-      style={{
-        background: `url(${org.bgurl}) no-repeat center`,
-        minWidth: '100%',
-        height: '350px',
-        borderRadius: '20px',
-        paddingTop: '20px',
-        boxShadow: '5px 5px 10px rgb(202, 202, 202)'
-      }}>
-      <button
-        type="button"
-        onKeyDown={showModal}
-        onClick={showModal}
-        style={{
-          backgroundColor: 'transparent',
-          border: 'none',
-          float: 'right',
-          marginRight: '10px',
-          cursor: 'pointer'
-        }}>
-        <img src="/SVG/org-view.svg" alt="view" style={{ width: '30px' }} />
-      </button>
-
-      <h1
-        style={{
-          color: 'white',
-          paddingTop: '55px',
-          marginLeft: '10px',
-          fontWeight: '500',
-          fontSize: '35px'
-        }}>
-        {org.organisation}
-      </h1>
-      <div
-        style={{
-          backgroundColor: 'white',
-          marginTop: '2px',
-          width: '100%',
-          height: '220px',
-          borderRadius: '20px'
-        }}>
-        <p
-          style={{
-            color: '#6D6D6D',
-            fontWeight: '300',
-            fontSize: '14px',
-            padding: '20px 20px',
-            display: '-webkit-box',
-            WebkitLineClamp: '3',
-            WebkitBoxOrient: 'vertical',
-            overflow: 'hidden'
-          }}>
-          {org.orgDescription}
-        </p>
-        <div>
-          <a href={org.github}>
-            <img
-              src="/SVG/org-github-icon.svg"
-              alt="github"
-              style={{
-                margin: ' 0 10px',
-                marginLeft: '25px',
-                marginTop: '20px',
-                cursor: 'pointer'
-              }}
-            />
-          </a>
-
-          <a href={org.slack}>
-            <img
-              src="/SVG/org-slack-icon.svg"
-              alt="slack"
-              style={{
-                margin: ' 0 10px',
-                marginTop: '20px',
-                cursor: 'pointer'
-              }}
-            />
-          </a>
-        </div>
-
-        <div className="Tags" style={{ marginLeft: '15px' }}>
-          {org.langTags.map((tag) => (
-            <p
-              key={tag}
-              style={{
-                display: 'inline-block',
-                borderRadius: '10px',
-                border: '1px black solid',
-                padding: '1px 5px',
-                fontWeight: '300',
-                margin: ' 10px 5px',
-                cursor: 'pointer'
-              }}>
+    <div className={styles['card-outer']}>
+      <div className={styles['top-row']}>
+        <h1 className={styles.heading}>{org.name}</h1>
+        <button
+          type="button"
+          onKeyDown={showModal}
+          onClick={showModal}
+          className={styles['show-modal-button']}>
+          <img src="/SVG/org-view.svg" alt="view" style={{ width: '30px' }} />
+        </button>
+      </div>
+      <div className={styles['card-inner']}>
+        <p>{org.short_description}</p>
+        <h4>Technologies</h4>
+        <div className={styles.tags}>
+          {tech.map((tag) => (
+            <div className={styles.tag} key={tag}>
               {tag}
-            </p>
+            </div>
           ))}
         </div>
+        <div className={styles.urls}>
+          <a href={org.website} target="_blank" rel="noopener noreferrer">
+            <img src="/icons/website.svg" alt="Website" />
+          </a>
+
+          <a href={org.idea_list_url} target="_blank" rel="noopener noreferrer">
+            <img src="/icons/idea.svg" alt="Ideas" />
+          </a>
+
+          <a
+            href={org.irc_channel_url}
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src="/icons/chat.svg" alt="Chat" />
+          </a>
+        </div>
       </div>
-      {ModalShow && <Modal org={org} hideModal={showModal} />}
+      {ModalShow && (
+        <Modal org={org} hideModal={showModal} topics={topics} tech={tech} />
+      )}
     </div>
   );
 };
 
 organisation.propTypes = {
-  org: PropTypes.exact({
-    organisation: PropTypes.string,
-    id: PropTypes.number,
-    link: PropTypes.string,
-    orgDescription: PropTypes.string,
-    langTags: PropTypes.arrayOf(PropTypes.string),
+  org: PropTypes.shape({
+    name: PropTypes.string,
+    website: PropTypes.string,
+    idea_list_url: PropTypes.string,
+    irc_channel_url: PropTypes.string,
+    short_description: PropTypes.string,
+    long_description: PropTypes.string,
     topicTags: PropTypes.arrayOf(PropTypes.string),
     github: PropTypes.string,
     slack: PropTypes.string,
     bgurl: PropTypes.string
-  }).isRequired
+  }).isRequired,
+  topics: PropTypes.arrayOf(PropTypes.string).isRequired,
+  tech: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default organisation;
